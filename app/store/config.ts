f805969d615ca9b1e0f587538625ca0c54ec666e
dragonforce2010 <coding.monkey.github@gmail.com>
2023-08-24 10:39:46 +0800

    adjust default window size

--- app/store/config.ts
+++ app/store/config.ts
@@ -17,10 +17,10 @@ export enum Theme {
 }
 
 export const DEFAULT_CONFIG = {
-  submitKey: SubmitKey.CtrlEnter as SubmitKey,
+  submitKey: SubmitKey.Enter as SubmitKey,
   avatar: "1f603",
   fontSize: 14,
-  theme: Theme.Auto as Theme,
+  theme: Theme.Light as Theme,
   tightBorder: false,
   sendPreviewBubble: true,
   sidebarWidth: 300,
